1ce3f32d5d916be5cf97a9d33441420e762c1d15
Akhterul Mustafa <amustafa2020@gmail.com>
2020-04-11 11:14:43 -0500

    First commit

---
+++ react-api/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>
+        <img src = ".\sogeti-med.png"  alt="logo" />
+        </p>
+        EKS: Auto Scale, Fault Tollerant, Reseliant, Self-Monitoring, Self-Healing
+        <p></p>
+        <p>
+        <img src = ".\k8new.png" className="App-logo" alt="logo" />
+        </p>
+       Dockerized React Application Running On Kubernetes
+      </header>
+    </div>
+  );
+}
+
+export default App;
